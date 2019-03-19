ae7530d4c0f136c38cbfe639d4efe0bca4fbf9ab
Parkitnyy <48156329+Parkitnyy@users.noreply.github.com>
2019-03-19 20:50:33 +0100

    Dodanie aktorów
    - Aktualizacja TODO
    - Nowy sposób zapisu danych (zapisujemy wszystkie dane)

--- imdbpyToCsv.py
+++ imdbpyToCsv.py
@@ -1,11 +1,10 @@
 # -------------------------------------------------------------------------------------------------------------------- #
 #                                                                                                                      #
-# TODO - Dodać aktorów                                                                                                 #
+# TODO - Naprawić zapisywanie                                                                                          #
 # TODO - Sprawdzic warunki try-except                                                                                  #
-# TODO - Plik dataKeywords.csv bez powtorzen (teoretycznie warunek jest dobry, ale czasem wartosci sie powtarzaja)     #
+# TODO - Plik dataKeywords.csv bez powtorzen (za trudne - Dawid to ogarnie)                                            #
 # TODO - Komentarze                                                                                                    #
 # TODO - Zmienne lepiej odzwierciedlajace przechowywane dane                                                           #
-# TODO - Usuniecie cudzyslowia z opisow (mozliwe, ze brakuje \ w tablicy znakow specjalnych)                           #
 #                                                                                                                      #
 # Jesli cos zrobicie to usuncie. Jak zrobicie wszystko z listy zostawcie naglowek i te wiadomosc                       #
 # ------------------------------------------ ELO MORDY --------------------------------------------------------------- #
@@ -13,6 +12,134 @@
 import numpy as np
 import pandas as pd
 from imdb import IMDb, IMDbError
+
+# --------------------------------------------- INICJALIZACJA ZMIENNYCH ---------------------------------------------- #
+i = 0  # reshape <-> rows
+rowInfo = list()  # tworzymy listy
+keywords = list()
+
+
+# ----------------------------------------------------- KONIEC ------------------------------------------------------- #
+
+def getInfo(  # pobieranie danych
+        simpleMovie,
+        i,
+        rowInfo,
+        keywords,
+        rowInfoLen=7,
+        boolTitle=True,
+        boolYear=True,
+        boolDirector=True,
+        boolRating=True,
+        boolGenre=True,
+        boolPlotmark=True,
+        boolActor=True
+):
+    try:
+        movie = ia.get_movie(simpleMovie)  # pobieramy info nt filmow o danym ID. Jak wywali Error -> wyświetla błąd ID
+        print(simpleMovie)
+        listOfInfos = list()  # Lista informacji, które będziemy przechowywać inforamcje na temat jednego filmu
+        if boolTitle:
+            listOfInfos.append(movie['title'])  # na początek tytułu
+        else:
+            listOfInfos.append("NULL")
+
+        if boolYear:
+            listOfInfos.append(movie['year'])  # potem rok produkcji
+        else:
+            listOfInfos.append("NULL")
+
+        if boolDirector:
+            listOfDirectors = list()  # tworzenie list
+            for director in movie['directors']:  # lista reżyserów, może być więcej niż jeden więc jest pętla
+                tempDirectors = director['name'].replace(' ', '_')  # usunięcie spacji i zamiana na _
+                listOfDirectors.append(tempDirectors)
+            listOfInfos.append(" ".join(listOfDirectors))
+        else:
+            listOfInfos.append("NULL")
+
+        if boolRating:
+            listOfInfos.append(movie['rating'])  # ocena filmu
+        else:
+            listOfInfos.append("NULL")
+
+        if boolGenre:
+            listOfInfos.append(" ".join(movie['genres']))  # kategoria
+        else:
+            listOfInfos.append("NULL")
+
+        if boolPlotmark:
+            # usuwamy .::, autora tekstu i znaki specjalne -- START
+            plots = list()
+            plotsmarks = list()
+            for plot in movie['plot']:
+                index = plot.find(".::")  # szukamy ".::" i od tego miejsca ucinamy recenzje
+                plot = plot[:index]
+                plots.append(plot)
+                for mark in marks:  # wyszukaj znaku interpunkcyjnego (lista marks) i go usun/zamien
+                    plot = plot.replace(mark, '')
+                plotsmarks.append(plot)
+            listOfInfos.append(" ".join(plotsmarks))
+            # usuwamy .::, autora tekstu i znaki specjalne  -- KONIEC
+        else:
+            listOfInfos.append("NULL")
+
+        if boolActor:
+            listOfActors = list()  # tworzenie listy
+            for actor in movie['cast']:  # lista reżyserów, może być więcej niż jeden więc jest pętla
+                tempActors = actor['name'].replace(' ', '_')  # usunięcie spacji i zamiana na _
+                listOfActors.append(tempActors)
+            listOfInfos.append(" ".join(listOfActors))
+        else:
+            listOfInfos.append("NULL")
+
+        if i == 0:
+            keywords = list()  # tworzymy liste słów kluczowych do danego ID
+            rowInfo = np.array(listOfInfos)  # tablica numpy (ma więcej metod)
+            rowInfoLen = len(rowInfo)  # ilosc info jaką pobraliśmy z filmu
+            i += 1  # powiększamy "i" czyli liczbę wierszy
+        else:
+            i += 1  # powiększamy "i" czyli liczbę wierszy
+            rowInfo = np.append(rowInfo, listOfInfos).reshape((i, rowInfoLen))  # zmien tablice numpy na numpy 2D
+
+        # keywords -- START
+        keywordsTemp = pd.read_csv("dataKeywords.csv", sep=";", index_col=0).values.transpose()
+        for word in listOfInfos[4].split():  # pętla która zapisuje wszystkie keywords
+            if word.casefold() not in keywordsTemp and word not in keywords:
+                keywords.append(word.casefold())
+                #  print(word.casefold())
+                #  open("temp.txt", "a+").write(word.casefold() + "\n")
+        for word in listOfInfos[5].split():  # taka sama pętla, ale inne info
+            if word.casefold() not in keywordsTemp and word not in keywords:
+                keywords.append(word.casefold())
+                #  print(word.casefold())
+                #  open("temp.txt", "a+").write(word.casefold() + "\n")
+        # keywords -- END
+
+    except IMDbError as e:
+        print(row, e)
+    except KeyError as e:
+        e = str(e)
+        if "title" in e:
+            boolTitle = False
+        elif "year" in e:
+            boolYear = False
+        elif "director" in e:
+            boolDirector = False
+        elif "rating" in e:
+            boolRating = False
+        elif "genre" in e:
+            boolGenre = False
+        elif "plot" in e:
+            boolPlotmark = False
+        elif "cast" in e:
+            boolActor = False
+        else:
+            print(e)
+            exit(-1)
+        print(e)
+        getInfo(simpleMovie, i, rowInfo, keywords, rowInfoLen, boolTitle, boolYear, boolDirector, boolRating,
+                boolGenre, boolPlotmark, boolActor)
 
 
 def saveIDs(stepIDList):  # funkcja do zapiswania już wykorzystanych ID do pliku
@@ -26,7 +153,14 @@ def saveIDs(stepIDList):  # funkcja do zapiswania już wykorzystanych ID do plik
 def saveToFile(argRowInfo, argKeywords, argDataSet, argDataKeywords):  # zapisywanie pobranych danych do pliku
     try:
         #  tworzenie dataFrame - taka tabelka która ma jasno określone nagłówki i indeksy
-        dataSetTemp = pd.DataFrame(argRowInfo, columns=['title', 'year', 'directors', 'rating', 'genres', 'plotmarks'])
+        dataSetTemp = pd.DataFrame(argRowInfo, columns=['title',
+                                                        'year',
+                                                        'directors',
+                                                        'rating',
+                                                        'genres',
+                                                        'plotmarks',
+                                                        'actors'
+                                                        ])
         dataKeywordsTemp = pd.DataFrame(argKeywords, columns=['keyword'])
 
         argDataSet = pd.concat([argDataSet, dataSetTemp])  # konkatenacja - łączenie tego co mamy w pliku i co pobrane
@@ -58,7 +192,7 @@ except FileNotFoundError as e:
 # unnecessary: 'cast', 'production companies',
 # print(ia.get_movie_infoset())  # args
 
-marks = {'.', ',', '<', '>', '/', '?', ';', ':', '\'', '\'s', '"', '[', '{', ']', '}', '!', '@', '#', '$', '%', '^',
+marks = {'.', ',', '<', '>', '/', '?', ';', ':', '\'', '\'s', '\"', '[', '{', ']', '}', '!', '@', '#', '$', '%', '^',
          '&', '&', '*', '(', ')', '-', '_', '=', '+'}  # lista znakow specjalnych do usunięcia
 
 lastId = str(0).zfill(7) + '\n'  # ustawione jako 0000001
@@ -73,15 +207,13 @@ except FileNotFoundError as e:
     open("dataSetIds.txt", "w+").write("")
 
 stopTerm = 0
-i = 0  # reshape <-> rows
-rowInfo = list()  # tworzymy listy
-keywords = list()
+
 idsToSave = list()
 
 try:
     dataSet = pd.read_csv("dataSet.csv", sep=";", index_col=0)  # czytaj plik .csv
 except FileNotFoundError as e:
-    open("dataSet.csv", "w+").write(";title;year;directors;rating;genres;plotmarks")  # jeśli go nie ma, to utwórz
+    open("dataSet.csv", "w+").write(";title;year;directors;rating;genres;plotmarks;actors")  # jeśli nie ma, to utwórz
     dataSet = pd.read_csv("dataSet.csv", sep=";", index_col=0)  # a potem czytaj
 
 try:
@@ -90,74 +222,22 @@ except FileNotFoundError as e:
     open("dataKeywords.csv", "w+").write(";keyword")  # jeśli go nie ma, to uwtórz
     dataKeywords = pd.read_csv("dataKeywords.csv", sep=";", index_col=0)  # a potem czytaj
 
-
 for row in fileR:  # kazdy imdbID z bazy
     if row > lastId:
-        try:
-            stopTerm += 1  # warunek stopu zapisu - kiedy ma zapisać
-            if stopTerm == 6:
-                print("Zapis do plikow")  # wyświetla że zapisuje
-                stopTerm = 0  # zerowanie warunku stopu
-                if saveToFile(rowInfo, keywords, dataSet, dataKeywords):  # wywołuje funkcje zapisu do pliku
-                    saveIDs(idsToSave)  # zapisuje
-                    idsToSave.clear()  # czyści - dupe na przykład
-                else:
-                    print("Niepowodzenie w zapisie!")  # błąd, wyświetla komunikat błedu
-                    exit(-1)  # kod błędu - kończenie programu
-            idsToSave.append(row)  # dopisue aktualne ID do listy ID przeznaczonych do zapisu
-
-            movie = ia.get_movie(row)  # pobieramy info nt filmow o danym ID. Jak wywali Error -> wyświetla błąd ID
-
-            listOfInfos = list()  # Lista informacji, które będziemy przechowywać inforamcje na temat jednego filmu
-            listOfInfos.append(movie['title'])  # na początek tytułu
-            listOfInfos.append(movie['year'])  # potem rok produkcji
-
-            listOfDirectors = list()  # tworzenie listy
-            listOfDirectorsSpace = list()  # tworzenie listy
-            for director in movie['directors']:  # lista reżyserów, może być więcej niż jeden więc jest pętla
-                tempDirectors = director['name'].replace(' ', '_')  # usunięcie spacji i zamiana na _
-                listOfDirectors.append(tempDirectors)
-            listOfInfos.append(" ".join(listOfDirectors))
-
-            listOfInfos.append(movie['rating'])  # ocena filmu
-            listOfInfos.append(" ".join(movie['genres']))  # kategoria
-
-            # usuwamy .::, autora tekstu i znaki specjalne -- START
-            plots = list()
-            plotsmarks = list()
-            for plot in movie['plot']:
-                index = plot.find(".::")  # szukamy ".::" i od tego miejsca ucinamy recenzje
-                plot = plot[:index]
-                plots.append(plot)
-                for mark in marks:  # wyszukaj znaku interpunkcyjnego (lista marks) i go usun/zamien
-                    plot = plot.replace(mark, '')
-                plotsmarks.append(plot)
-            listOfInfos.append(" ".join(plotsmarks))
-            # usuwamy .::, autora tekstu i znaki specjalne  -- KONIEC
-
-            if i == 0:
-                keywords = list()  # tworzymy liste słów kluczowych do danego ID
-                rowInfo = np.array(listOfInfos)  # tablica numpy (ma więcej metod)
-                rowInfoLen = len(rowInfo)  # ilosc info jaką pobraliśmy z filmu
-                i += 1  # powiększamy "i" czyli liczbę wierszy
+        stopTerm += 1  # warunek stopu zapisu - kiedy ma zapisać
+        if stopTerm == 6:
+            print("Zapis do plikow")  # wyświetla że zapisuje
+            stopTerm = 0  # zerowanie warunku stopu
+            if saveToFile(rowInfo, keywords, dataSet, dataKeywords):  # wywołuje funkcje zapisu do pliku
+                saveIDs(idsToSave)  # zapisuje
+                idsToSave.clear()  # czyści - dupe na przykład
             else:
-                i += 1  # powiększamy "i" czyli liczbę wierszy
-                rowInfo = np.append(rowInfo, listOfInfos).reshape((i, rowInfoLen))  # zmien tablice numpy na numpy 2D
+                print("Niepowodzenie w zapisie!")  # błąd, wyświetla komunikat błedu
+                exit(-1)  # kod błędu - kończenie programu
 
-            # keywords -- START
-            keywordsTemp = pd.read_csv("dataKeywords.csv", sep=";", index_col=0).values.transpose()
-            for word in listOfInfos[4].split():  # pętla która zapisuje wszystkie keywords
-                if word.casefold() not in keywordsTemp and word not in keywords:
-                    keywords.append(word.casefold())
-            for word in listOfInfos[5].split():  # taka sama pętla, ale inne info
-                if word.casefold() not in keywordsTemp and word not in keywords:
-                    keywords.append(word.casefold())
-            # keywords -- END
+        idsToSave.append(row)  # dopisue aktualne ID do listy ID przeznaczonych do zapisu
 
-        except IMDbError as e:
-            print(row, e)
-        except KeyError as e:
-            print("Brak pelnych danych filmu o ID: ", row, "; brakujace dane: ", e)
+        getInfo(row, i, rowInfo, keywords)
 
 if saveToFile(rowInfo, keywords, dataSet, dataKeywords):
     saveIDs(idsToSave)
